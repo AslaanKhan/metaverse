c3a3e35c0aa3ac5c7168232281a70117f1b72900
Arsalaan Khan <arslaan.tech@gmail.com>
2024-11-11 18:57:48 +0530

    first commit

---
+++ apps/http/src/middleware/admin.ts
@@ -0,0 +1,36 @@
+declare global {
+    namespace Express {
+        export interface Request {
+            role?: "Admin" | "User"
+            userId?: string
+        }
+    }
+}
+
+
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../config";
+import e, { NextFunction, Request, Response } from "express";
+
+export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const token = header?.split(" ")[1];
+    
+    if (!token) {
+        res.status(403).json({message: "Unauthorized"})
+        return
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_PASSWORD) as { role: string, userId: string }
+        if (decoded.role !== "Admin") {
+            res.status(403).json({message: "Unauthorized"})
+            return
+        }
+        req.userId = decoded.userId
+        next()
+    } catch(e) {
+        res.status(401).json({message: "Unauthorized"})
+        return
+    }
+}
